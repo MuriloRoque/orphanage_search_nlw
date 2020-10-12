5df998f55b4422d351c553ef1ba0b5e7d449e36d
Murilo Roque <muriloengqui@gmail.com>
2020-10-12 10:50:33 -0300

    implement initial orphanage-map design page

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import MapMarkerImg from '../images/map-marker.svg';
+import { FiPlus } from 'react-icons/fi';
+import '../styles/pages/orphanages-map.css';
 
 const OrphanagesMap = () => {
   return(
-    <h1>Hello World</h1>
+    <div id='page-map'>
+      <aside>
+        <header>
+          <img src={MapMarkerImg} alt=""/>
+          <h2>Choose an orphanage in the map</h2>
+          <p>A lot of children are waiting for your visit :)</p>
+        </header>
+        <footer>
+          <strong>Ribeirão Preto</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+      <div></div>
+      <Link to='' className='create-orphanage'>
+        <FiPlus size={32} color='#fff'/>
+      </Link>
+    </div>
   )
 }
 
