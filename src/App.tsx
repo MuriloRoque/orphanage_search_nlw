70c535d9d15b9fb3daf42156c3f72a3b7638e11b
Murilo Roque <muriloengqui@gmail.com>
2020-10-12 10:11:50 -0300

    implement landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
 
 import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id='page-landing'>
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+        <main>
+          <h1>Bring happyness to the world</h1>
+          <p>Visit orphanages and change many children's day</p>
+        </main>
+        <div className="location">
+          <strong>Ribeirão Preto</strong>
+          <span>São Paulo</span>
+        </div>
+        <a href="#" className='enter-app'>
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
     </div>
   );
 }
