5a1ec46387ba7ebe2a4d571d9a10743367b5e254
Murilo Roque <muriloengqui@gmail.com>
2020-10-13 10:23:07 -0300

    implement index route and controller

--- src/routes.ts
+++ src/routes.ts
@@ -4,5 +4,6 @@ import OrphanagesController from './controllers/OrphanagesController';
 const routes = Router();
 
 routes.post('/orphanages', OrphanagesController.create);
+routes.get('/orphanages', OrphanagesController.index);
 
 export default routes;
