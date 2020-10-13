a8d85ffb50e17babd709ebad7fc55e7ed21a73cd
Murilo Roque <muriloengqui@gmail.com>
2020-10-13 10:53:54 -0300

    configure multer to allow images uploads

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import OrphanagesController from './controllers/OrphanagesController';
+import multer from 'multer';
+import uploadConfig from './config/upload';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
-routes.post('/orphanages', OrphanagesController.create);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
 
