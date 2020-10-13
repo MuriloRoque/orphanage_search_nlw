43add8a05da6785ed3acef9d76bd90ef935ae424
Murilo Roque <muriloengqui@gmail.com>
2020-10-13 11:07:55 -0300

    implement image model with relationships to orphanage

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -33,6 +33,11 @@ export default {
     } = request.body;
   
     const orphanagesRepository = getRepository(Orphanage);
+
+    const requestImages = request.files as Express.Multer.File[];
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    })
   
     const orphanage = orphanagesRepository.create({
       name,
@@ -42,6 +47,7 @@ export default {
       instructions,
       opening_hours,
       open_on_weekends,
+      images,
     });
   
     await orphanagesRepository.save(orphanage);
