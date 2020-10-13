d154b131d2f3cfe40086ac3205ca8faf81643bd4
Murilo Roque <muriloengqui@gmail.com>
2020-10-13 10:25:51 -0300

    implement show route and controller action

--- src/routes.ts
+++ src/routes.ts
@@ -5,5 +5,6 @@ const routes = Router();
 
 routes.post('/orphanages', OrphanagesController.create);
 routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
 
 export default routes;
