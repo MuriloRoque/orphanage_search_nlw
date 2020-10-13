bc7380c56fad4d0ad7f297be1ef2ddd73625198d
Murilo Roque <muriloengqui@gmail.com>
2020-10-13 11:22:33 -0300

    implement data views

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import path from 'path';
+
 import './database/connection';
 
 import routes from './routes';
@@ -9,5 +11,6 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.listen(3333);
