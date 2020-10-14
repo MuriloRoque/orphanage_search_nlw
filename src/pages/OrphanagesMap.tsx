5a2e2d83ce96db6573119a2e4714f7d396641685
Murilo Roque <muriloengqui@gmail.com>
2020-10-14 11:01:02 -0300

    refactor map icon

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -4,15 +4,7 @@ import MapMarkerImg from '../images/map-marker.svg';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import '../styles/pages/orphanages-map.css';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import Leaflet from 'leaflet';
-
-const mapIcon = Leaflet.icon({
-  iconUrl: MapMarkerImg,
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [170, 2],
-})
+import mapIcon from '../utils/mapIcon';
 
 const OrphanagesMap = () => {
   return(
